007bd09e7f5d82a9a1847b01730a0ef1b667be0c
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-11-28 05:45:21 +0200

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,10 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import './video-react.css';
-import NavBar from '@/components/header/NavBar';
+// import NavBar from '@/components/header/NavBar';
 import Footer from '@/components/home/Footer';
 import '../style/main.css';
-import { Navigation } from 'lucide-react';
+// import { Navigation } from 'lucide-react';
 <link rel="stylesheet" href="/css/video-react.css" />
 
 
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" id='root'>
       <body className="w-full" suppressHydrationWarning={true}>
-        <NavBar />
+        {/* <NavBar /> */}
         {children}
         <Footer />
       </body>
