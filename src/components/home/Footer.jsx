c588cb278e28ef479f7f8cbd2a619656516cf9e8
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-11-30 12:22:32 +0200

    f

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -27,7 +27,8 @@ export default function Footer() { // Defining the main functional component nam
                     </div>
                     <div className="social">
                         <Link href={'https://x.com/mzarapp'}><i className="fa-brands fa-x-twitter"></i></Link>
-                        <Link href={'https://www.instagram.com/mzarapp//'}><i className="fa-brands fa-instagram"></i></Link>
+                        <Link href={'https://www.instagram.com/mzarapp/'}><i className="fa-brands fa-instagram"></i></Link>
+                        
                         <Link href={'https://www.facebook.com/mzarapp'}><i className="fa-brands fa-facebook"></i></Link>
                         <Link href={'https://www.youtube.com/@mzarapp'}><i className="fa-brands fa-youtube"></i></Link>
                         <Link href={'https://www.tiktok.com/@mzarapp'}><i className="fa-brands fa-tiktok"></i></Link>
